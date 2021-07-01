2ff540a87f5cee083017dbde88e0a16b31211656
Alexandr Shabalkin <sashatlt8@gmail.com>
2021-07-01 23:59:45 +0400

    DR-01 url minor fixes

--- django_rolls/apps/users/urls.py
+++ django_rolls/apps/users/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import include, url
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from django_rolls.api.endpoint import UserViewSet
+from .views import lk
 
-router = DefaultRouter()
+api_router = DefaultRouter()
+api_router.register(r'users', UserViewSet)
 
-router.register(r'users', UserViewSet)
+api_urlpatterns = [
+    url(r'^', include(api_router.urls)), # User view sets
+    url(r'auth', views.obtain_auth_token) # User auth token
+]
 
-urlpatterns = [
-    url(r'^', include(router.urls)),
+view_urlpatterns = [
+    url('', lk)
 ]
--- django_rolls/urls.py
+++ django_rolls/urls.py
@@ -1,26 +1,24 @@
-from rest_framework.authtoken import views
-
 from django.conf import settings
 from django.conf.urls import url
 from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic.base import RedirectView
 
 from django_rolls.apps.users import urls as user_router_urls
 
 
-api_endpoint_view = [
-    url(r'', include(user_router_urls)),
+api_endpoints = [
+    path('', include(user_router_urls.api_urlpatterns)),
+]
+
+view_endpoints = [
+    path('lk', include(user_router_urls.view_urlpatterns))
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api-token-auth/', views.obtain_auth_token), # AUTHORIZATION
-    path('api/v1/', include(api_endpoint_view)), # API
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
+    path('api/v1/', include(api_endpoints)), # API
+    path('', include(view_endpoints)), # VIEW TEMPLATES
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
